01983bb6595f5fbed4908db5d4436c08023fc96c
phaedraa <phaedra.a.r@gmail.com>
2016-10-22 10:21:02 -0700

    Add dynamic port

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Matchability app listening on port 3000!');
+app.listen(port, function() {
+  console.log('Matchability app listening on port ' + port);
 });
