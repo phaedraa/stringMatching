59ace2e52362aa407e53f00781e37edc5b957b1e
phaedraa <phaedra.a.r@gmail.com>
2016-10-21 23:39:02 -0700

    Add express server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Matchability app listening on port 3000!');
+});
