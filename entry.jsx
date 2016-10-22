6a4892778b58c5c189136d0a9552d8f6cc5241d4
phaedraa <phaedra.a.r@gmail.com>
2016-10-21 22:11:11 -0700

    Remove GridList and Add Title

--- entry.jsx
+++ entry.jsx
@@ -4,10 +4,9 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import TextField from 'material-ui/TextField';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import {GridList, GridTile} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import FanIcon from 'material-ui/svg-icons/hardware/toys';
 import matchability from './matchability';
+import AppBar from 'material-ui/AppBar';
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
@@ -36,39 +35,28 @@ class Form extends React.Component {
     return (
       <MuiThemeProvider>
     		<div>
-					<GridList
-						cols={2}
-						cellHeight={200}
-						padding={4}>
-						<GridTile
-							key="text-tile"
-							cols={2}
-							rows={1}>
-							<TextField
-								name="string-one"
-								placeholder="String one"
-								errorText="String one is required"
-								value={this.state.value}
-								onChange={this.handleChangeStringOne} />
-			        <TextField
-								name="string-two"
-								placeholder="String two"
-								errorText="String two is required"
-								value={this.state.value}
-								onChange={this.handleChangeStringTwo} />
-						</GridTile>
-						<GridTile
-              key="button-tile"
-              cols={1}
-              rows={1}>
-              <RaisedButton
-		            label="Go"
-		            labelPosition="before"
-		            primary={true}
-			          onClick={this.handleSubmit}
-		          />
-		        </GridTile>
-	        </GridList>
+          <AppBar
+            title="String Matchability Calculator"
+            showMenuIconButton={false}
+          />
+					<TextField
+						name="string-one"
+						placeholder="String one"
+						errorText="String one is required"
+						value={this.state.value}
+						onChange={this.handleChangeStringOne} />
+	        <TextField
+						name="string-two"
+						placeholder="String two"
+						errorText="String two is required"
+						value={this.state.value}
+						onChange={this.handleChangeStringTwo} />
+          <RaisedButton
+            label="Go"
+            labelPosition="before"
+            primary={true}
+	          onClick={this.handleSubmit}
+          />
 	      </div>
 	    </MuiThemeProvider>
     );
