4a7a25e58c8af5006264d213d3eea5da2dc82a26
phaedraa <phaedra.a.r@gmail.com>
2016-10-22 00:00:32 -0700

    Use old node notation

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(app.get('port') || 3000, function() {
-  console.log('Matchability app listening on port ' + app.get('port'));
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Matchability app listening on port ' + process.env.PORT);
 });
