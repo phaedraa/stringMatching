414f56991a8527b4358065eb348cfe4f1da96f4f
phaedraa <phaedra.a.r@gmail.com>
2016-10-21 22:30:43 -0700

    Style componenents and add subheader

--- entry.jsx
+++ entry.jsx
@@ -6,6 +6,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import matchability from './matchability';
 import AppBar from 'material-ui/AppBar';
+import Subheader from 'material-ui/Subheader';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -32,6 +33,11 @@ class Form extends React.Component {
   }
 
   render() {
+    const buttonContainerStyle = {
+      padding: "30px",
+      display: "flex",
+      justifyContent: "space-between",
+    };
     return (
       <MuiThemeProvider>
     		<div>
@@ -39,24 +45,36 @@ class Form extends React.Component {
             title="String Matchability Calculator"
             showMenuIconButton={false}
           />
-					<TextField
-						name="string-one"
-						placeholder="String one"
-						errorText="String one is required"
-						value={this.state.value}
-						onChange={this.handleChangeStringOne} />
-	        <TextField
-						name="string-two"
-						placeholder="String two"
-						errorText="String two is required"
-						value={this.state.value}
-						onChange={this.handleChangeStringTwo} />
-          <RaisedButton
-            label="Go"
-            labelPosition="before"
-            primary={true}
-	          onClick={this.handleSubmit}
-          />
+          <Subheader>
+            This program takes two strings as inputs and outputs two numbers, N1
+             and N2. The output describes a degree of matching between the two
+             strings: N1 represents the number of letters in one string that
+             each match in value and in position to a letter in the other
+             string. N2 represents the number of letters in one string that each
+             match in value, but not in position to a letter in the other
+             string.
+          </Subheader>
+          <div style={buttonContainerStyle}>
+  					<TextField
+  						name="string-one"
+  						placeholder="String one"
+  						errorText="String one is required"
+  						value={this.state.value}
+  						onChange={this.handleChangeStringOne} />
+  	        <TextField
+  						name="string-two"
+  						placeholder="String two"
+  						errorText="String two is required"
+  						value={this.state.value}
+  						onChange={this.handleChangeStringTwo} />
+            <RaisedButton
+              label="Go"
+              labelPosition="before"
+              primary={true}
+  	          onClick={this.handleSubmit}
+              style={{boxShadow: ''}}
+            />
+          </div>
 	      </div>
 	    </MuiThemeProvider>
     );
